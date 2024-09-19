b86ae61e502ea11d2bb3ba9cce5534098ff448df
galih fathurrochman <112954226+Galihfath@users.noreply.github.com>
2024-09-19 06:43:10 +0000

    Deskripsi perubahan

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,21 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Aktifkan CORS agar backend bisa menerima request dari domain lain
-app.use(cors());
+// Aktifkan CORS untuk semua origin (untuk sementara)
+app.use(cors()); // Mengizinkan semua origin sementara
+
+// Middleware untuk menangani request body dalam format JSON
+app.use(express.json());
 
 // Route untuk root path "/"
 app.get('/', (req, res) => {
   res.send('siDORA Backend Running!');
 });
 
-// Route untuk "/api" yang mengembalikan JSON
+// Route untuk "/api" yang mengirimkan respons JSON
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from siDORA Backend!' });
-});
+});;
 
 // Menjalankan server di port 5000
 app.listen(PORT, () => {
